8ff8d8c3dc65adc33055af43b8ee4ea6c0c19542
Jared McFarland <jared@mixpanel.com>
2022-05-05 18:05:26 -0700

    Version 1.4.8

--- lib/mixpanel_flutter.dart
+++ lib/mixpanel_flutter.dart
@@ -8,7 +8,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class Mixpanel {
   static const MethodChannel _channel = const MethodChannel('mixpanel_flutter');
   static Map<String, String> _mixpanelProperties = {
-    '\$lib_version': '1.4.7',
+    '\$lib_version': '1.4.8',
     'mp_lib': 'flutter',
   };
 
--- test/mixpanel_flutter_test.dart
+++ test/mixpanel_flutter_test.dart
@@ -35,7 +35,7 @@ void main() {
             'token': "test token",
             'optOutTrackingDefault': false,
             'mixpanelProperties': {
-              '\$lib_version': '1.4.7',
+              '\$lib_version': '1.4.8',
               'mp_lib': 'flutter',
             },
             'superProperties': null,
@@ -55,7 +55,7 @@ void main() {
             'token': "test token",
             'optOutTrackingDefault': true,
             'mixpanelProperties': {
-              '\$lib_version': '1.4.7',
+              '\$lib_version': '1.4.8',
               'mp_lib': 'flutter',
             },
             'superProperties': null,
