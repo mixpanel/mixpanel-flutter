bfd50052584b4cc91fffc2f9efde55f87c5a1af6
Jared McFarland <jared@mixpanel.com>
2022-05-09 16:06:26 -0700

    Version 1.5.0

--- lib/mixpanel_flutter.dart
+++ lib/mixpanel_flutter.dart
@@ -10,7 +10,7 @@ class Mixpanel {
   static const MethodChannel _channel = const MethodChannel(
       'mixpanel_flutter', StandardMethodCodec(MixpanelMessageCodec()));
   static Map<String, String> _mixpanelProperties = {
-    '\$lib_version': '1.4.8',
+    '\$lib_version': '1.5.0',
     'mp_lib': 'flutter',
   };
 
