5496543954f878e9e6a318d0632772bb8416ea3c
Nick van der Waal <nick.vanderwaal@afas.nl>
2026-03-16 11:24:49 +0100

    revert Registrar deletion

--- android/src/main/java/com/mixpanel/mixpanel_flutter/MixpanelFlutterPlugin.java
+++ android/src/main/java/com/mixpanel/mixpanel_flutter/MixpanelFlutterPlugin.java
@@ -19,6 +19,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
 
