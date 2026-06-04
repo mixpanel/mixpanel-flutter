bbaa0e1a3d9c6ec261abbc2cbcde146ff011af5d
Theo Germain <theo.germain@icloud.com>
2026-06-04 16:19:43 +0200

    fix(spm): bump iOS minimum deployment target to 13.0 in Package.swift

--- packages/mixpanel_flutter/ios/mixpanel_flutter/Package.swift
+++ packages/mixpanel_flutter/ios/mixpanel_flutter/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "mixpanel_flutter",
     platforms: [
-        .iOS("12.0")
+        .iOS("13.0")
     ],
     products: [
         .library(
